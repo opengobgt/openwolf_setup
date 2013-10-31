588bbf94c3d8e107bca0174aebba808ce8426ce0
Javier Alvarez <jalvarezsamayoa@gmail.com>
2013-10-31 08:48:44 -0600

    Update boostrap.sh

--- boostrap.sh
+++ boostrap.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 
 # Instlacion Puppet
 cd ~
@@ -12,10 +12,11 @@ sudo apt-get update
 sudo apt-get install puppet -y
 
 # Instalacion emacs
-sudo apt-get install python-software-properties
+sudo apt-get install python-software-properties -y
 sudo add-apt-repository ppa:cassou/emacs
 sudo apt-get update
-sudo apt-get install emacs24
+sudo apt-get install emacs24 -y
+sudo apt-get install git-core -y
 
 
 
